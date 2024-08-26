7b49ad86a91406b0e767cdf97fee489d064f6652
MotiurRafi <motiurrafi601@gmail.com>
2024-08-26 21:25:52 +0600

    socket update

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -33,7 +33,7 @@ export default function Item({
       fetchItem(urlId);
       fetchComment(urlId)
     }
-    socketRef.current = io('wss://collection-management-backend.onrender.com:8080', {
+    socketRef.current = io('wss://collection-management-backend.onrender.com', {
       path: '/socket.io',
       transports: ['websocket'],
     });
